0a0b7d2466d3cb00c85e7441af45b411e12d9d6e
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-02-15 10:29:54 -0600

    Add commands in web

--- lab1/intro_js.js
+++ lab1/intro_js.js
@@ -4,7 +4,18 @@ console.warn("WARNING!");
 console.error("ERRORR!!");
 console.assert(1==1);
 console.assert(1==2);
+alert("adios");
 
+const nombre = prompt("Como te llamas");
+console.log(`Bienvenido` + nombre);
+
+const hambre = confirm("Tienes hambre");
+
+if(hambre){
+    alert("Buen provecho");
+    }else{
+        console.log("a chambear");
+    }
 
 //forma antigua
 var  juego = "Minecraft";
