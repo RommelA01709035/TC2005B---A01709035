bb09086c1e0a66b50c49df04bb7399aa109e9e2d
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-02-19 10:12:19 -0600

    add code of lalo

--- lab1/bulma.js
+++ lab1/bulma.js
@@ -1,14 +1,29 @@
-console.log("ya se cargo owo");
 const boton = document.getElementById("boton_construir");
-console.log(boton);
-boton.onclick = () => { 
-    console.log("click"); 
-    const imagen = document.getElementById("construyendo");
+
+const construir_casa = () => { 
+    const imagen = document.getElementById("imagen_construir");
     imagen.src = "https://www.movilzona.es/app/uploads-movilzona.es/2022/02/casa-minecraft-inicio-primera-planta.jpg";
     
-    boton.innerHTML = "Terminar Casa";
+    boton.innerHTML = "Terminar casa";
     boton.className = "button is-warning";
-    console.log(imagen);
+    boton.onclick = terminar_casa;
+};
+
+const terminar_casa = () => { 
+    const imagen = document.getElementById("imagen_construir");
+    imagen.src = "https://i.blogs.es/7cfcd0/casas-en-minecraft/1366_2000.jpeg";
     
+    boton.innerHTML = "Destruir casa";
+    boton.className = "button is-danger";
+    boton.onclick = destruir_casa;
+};
+
+const destruir_casa = () => { 
+    const imagen = document.getElementById("imagen_construir");
+    imagen.src = "https://minecraft.tools/es/css/img/index/02.jpg";
+    
+    boton.innerHTML = "Construir casa";
+    boton.className = "button is-primary";
+    boton.onclick = construir_casa;
 };
 
