3b9b37ee92d65f4362663174a8b8fd54d520da0b
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-02-19 09:55:59 -0600

    add scripts and swap images

--- lab1/bulma.js
+++ lab1/bulma.js
@@ -1,6 +1,14 @@
 console.log("ya se cargo owo");
 const boton = document.getElementById("boton_construir");
 console.log(boton);
-boton.onclick = () => {
-    console.log("click")
+boton.onclick = () => { 
+    console.log("click"); 
+    const imagen = document.getElementById("construyendo");
+    imagen.src = "https://www.movilzona.es/app/uploads-movilzona.es/2022/02/casa-minecraft-inicio-primera-planta.jpg";
+    
+    boton.innerHTML = "Terminar Casa";
+    boton.className = "button is-warning";
+    console.log(imagen);
+    
 };
+
