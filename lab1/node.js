9b6f8be7e4ae44619558114e660e6472f750daa5
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-02-22 10:22:56 -0600

    add server in node and delete unnecesary directory

--- lab1/node.js
+++ lab1/node.js
@@ -4,11 +4,24 @@ console.log("Hola desde node");
 const filesystem = require('fs'); // es más eficiente el preprocesamiento
 filesystem.writeFileSync('hola.txt','Hola desde node otra vez') //"" son para coficiar adentro
 //chmode da permisos a un archivos
+const hackeo = () =>{
+    console.log("te hackie pa");
+};
 
 const arreglo = [5000, 30, 1000, 10000];
-//Asincrono no es secuencial
+//Asincrono no es secuencial, primero mete el codigo de tiempo en la linea 
+
 for(let item of arreglo){
     setTimeout(()=>{
         console.log(item);
     }, item);
 }
+
+
+setTimeout(hackeo, 7000);
+
+//http es un modulo d enode con las funciones de un servidor web
+const http = require('http');
+
+
+console.log("Esto se imrpime primero antes que la linea");
