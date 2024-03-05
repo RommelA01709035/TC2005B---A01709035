c5356c3eb38cf547942ca19c7069c42d1172d1b2
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-03-05 10:09:10 -0600

    obtener valor de una cookie desde un controlador

--- lab1/controllers/construcciones.controller.js
+++ lab1/controllers/construcciones.controller.js
@@ -6,16 +6,26 @@ exports.get_construir = (request, response, next) => {
 
 exports.post_construir = (request, response, next) => {
     console.log(request.body);
-    const construccion =  new Construccion(
-        request.body.nombre, request.body.imagen
-    )
-    construccion.save()
+    const construccion = 
+    new Construccion(request.body.nombre, request.body.imagen);
+    construccion.save();
+    
+    response.setHeader('Set-Cookie', 'ultima_construccion=' + request.body.nombre);
     response.redirect('/');
 };
 
+
 exports.get_root = (request, response, next) => {
     console.log('Ruta /');
+    let ultima_construccion = request.get('Cookie');
+    if (ultima_construccion) {
+        ultima_construccion = ultima_construccion.split('=')[1];
+    } else {
+        ultima_construccion = '';
+    }
+    console.log(ultima_construccion);
     response.render('construcciones', {
         construcciones: Construccion.fetchAll(),
+        ultima_construccion: ultima_construccion,
     });
 }
